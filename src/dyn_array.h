427a5e839b0aea7e84673c4ffab624afc5a84909
Johan Smet <johan.smet@justcode.be>
2018-04-05 22:34:00 +0200

    Add extra functions for dynamic arrays.
    arr_printf: use the a char array as a auto-sizing string
    arr_buf_push: push several elements at once

--- src/dyn_array.h
+++ src/dyn_array.h
@@ -8,7 +8,9 @@
 #define JS_DYN_ARRAY_H
 
 #include <stdlib.h>
+#include <string.h>
 #include <stddef.h>
+#include <stdarg.h>
 #include <assert.h>
 
 typedef struct ArrayHeader {
@@ -24,9 +26,11 @@ typedef struct ArrayHeader {
 #define arr_len(a) ((a) ? arr__hdr((a))->len : 0)
 #define arr_cap(a) ((a) ? arr__hdr((a))->cap : 0)
 #define arr_push(a, i) (arr__fit((a), 1), (a)[arr__hdr(a)->len++] = (i))
+#define arr_push_buf(a, b, n) (arr__fit((a), n), memcpy(arr_end(a), (b), (n) * sizeof(*(a))), arr__hdr(a)->len+= (n))
 #define arr_free(a) ((a) ? (free(arr__hdr(a)), (a) = NULL) : 0)
 #define arr_clear(a) ((a) ? arr__hdr((a))->len = 0 : 0)
 #define arr_end(a) ((a) + arr_len(a))
+#define arr_printf(a, ...) (a) = arr__printf((a), __VA_ARGS__)
 
 static void *arr__grow(const void *array, size_t new_len, size_t elem_size) {
     size_t dbl_cap = arr_cap(array) * 2;
@@ -41,6 +45,27 @@ static void *arr__grow(const void *array, size_t new_len, size_t elem_size) {
     }
     new_hdr->cap = new_cap;
     return new_hdr->data;
+}
+
+static char *arr__printf(char *array, const char *fmt, ...) {
+    va_list args;
+    size_t avail = arr_cap(array) - arr_len(array);
+    va_start(args, fmt);
+    size_t n = vsnprintf(arr_end(array), avail, fmt, args) + 1;
+    va_end(args);
+
+    if (n > avail) {
+        // array too small, grow and print again
+        arr__fit(array, n);
+        size_t avail = arr_cap(array) - arr_len(array);
+        va_start(args, fmt);
+        n = vsnprintf(arr_end(array), avail, fmt, args) + 1;
+        va_end(args);
+    }
+
+    // don't count zero-terminator in array length so it concatenates easily (terminator is always present!)
+    arr__hdr(array)->len += n - 1;
+    return array;
 }
 
 #endif // JS_DYN_ARRAY_H
