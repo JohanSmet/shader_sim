56f8e3585a290662419ef278d18ad6c4888b5ad0
Johan Smet <johan.smet@justcode.be>
2018-07-15 22:35:14 +0200

    spirv_simulator: basic math operators

--- src/spirv_simulator.c
+++ src/spirv_simulator.c
@@ -7,6 +7,7 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <math.h>
 
 static VariableData *new_variable_data(uint8_t *data, size_t size) {
     VariableData *result = (VariableData *) malloc(sizeof(VariableData));
@@ -118,6 +119,36 @@ size_t spirv_register_to_string(SPIRV_simulator *sim, uint32_t reg_idx, char *ou
         assert(sim);    \
         assert(op);
 
+#define OP_FUNC_RES_1OP(kind) \
+    OP_FUNC_BEGIN(kind)       \
+        uint32_t result_type = op->optional[0];     \
+        uint32_t result_id = op->optional[1];       \
+        uint32_t operand = op->optional[2];         \
+                                                    \
+        /* assign a register to keep the data */    \
+        Type *res_type = spirv_module_type_by_id(sim->module, result_type);     \
+        uint32_t res_reg = spirv_sim_assign_register(sim, result_id, res_type); \
+                                                                                \
+        /* retrieve register used for operand */                                \
+        uint32_t op_reg = map_int_int_get(&sim->assigned_regs, operand);
+
+
+#define OP_FUNC_RES_2OP(kind) \
+    OP_FUNC_BEGIN(kind)       \
+        uint32_t result_type = op->optional[0];     \
+        uint32_t result_id = op->optional[1];       \
+        uint32_t op1_id = op->optional[2];          \
+        uint32_t op2_id = op->optional[3];          \
+                                                    \
+        /* assign new register for the result */    \
+        Type *res_type = spirv_module_type_by_id(sim->module, result_type);     \
+        uint32_t res_reg = spirv_sim_assign_register(sim, result_id, res_type); \
+                                                                                \
+        /* retrieve register used for operand */                                \
+        uint32_t op1_reg = map_int_int_get(&sim->assigned_regs, op1_id);        \
+        uint32_t op2_reg = map_int_int_get(&sim->assigned_regs, op2_id);
+
+
 #define OP_FUNC_END   }
 
 OP_FUNC_BEGIN(SpvOpLoad)
@@ -167,10 +198,212 @@ OP_FUNC_BEGIN(SpvOpStore)
         return;
     }
 
-
     // copy data
     size_t var_size = res_type->count * res_type->size;
     memcpy(data->buffer, sim->temp_regs[reg].vec, var_size);
+
+OP_FUNC_END
+
+/*
+ * arithmetic instructions
+ */
+
+OP_FUNC_RES_1OP(SpvOpSNegate)
+
+    assert(spirv_sim_type_is_integer(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].svec[i] = -sim->temp_regs[op_reg].svec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_1OP(SpvOpFNegate)
+
+    assert(spirv_sim_type_is_float(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].vec[i] = -sim->temp_regs[op_reg].vec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpIAdd)
+
+    assert(spirv_sim_type_is_integer(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].svec[i] = sim->temp_regs[op1_reg].svec[i] + sim->temp_regs[op2_reg].svec[i];
+}
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpFAdd)
+
+    assert(spirv_sim_type_is_float(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].vec[i] = sim->temp_regs[op1_reg].vec[i] + sim->temp_regs[op2_reg].vec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpISub)
+
+    assert(spirv_sim_type_is_integer(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].svec[i] = sim->temp_regs[op1_reg].svec[i] - sim->temp_regs[op2_reg].svec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpFSub)
+
+    assert(spirv_sim_type_is_float(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].vec[i] = sim->temp_regs[op1_reg].vec[i] - sim->temp_regs[op2_reg].vec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpIMul)
+
+    assert(spirv_sim_type_is_integer(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].svec[i] = sim->temp_regs[op1_reg].svec[i] * sim->temp_regs[op2_reg].svec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpFMul)
+
+    assert(spirv_sim_type_is_float(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].vec[i] = sim->temp_regs[op1_reg].vec[i] * sim->temp_regs[op2_reg].vec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpUDiv)
+
+    assert(spirv_sim_type_is_integer(res_type->kind));
+    assert(!res_type->is_signed);
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].uvec[i] = sim->temp_regs[op1_reg].uvec[i] / sim->temp_regs[op2_reg].uvec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpSDiv)
+
+    assert(spirv_sim_type_is_integer(res_type->kind));
+    assert(res_type->is_signed);
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].svec[i] = sim->temp_regs[op1_reg].svec[i] / sim->temp_regs[op2_reg].svec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpFDiv)
+
+    assert(spirv_sim_type_is_float(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].vec[i] = sim->temp_regs[op1_reg].vec[i] / sim->temp_regs[op2_reg].vec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpUMod)
+
+    assert(spirv_sim_type_is_integer(res_type->kind));
+    assert(!res_type->is_signed);
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].uvec[i] = sim->temp_regs[op1_reg].uvec[i] / sim->temp_regs[op2_reg].uvec[i];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpSRem)
+/* Signed remainder operation of Operand 1 divided by Operand 2. The sign of a non-0 result comes from Operand 1. */
+
+    assert(spirv_sim_type_is_integer(res_type->kind));
+    assert(res_type->is_signed);
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        int32_t v1 = sim->temp_regs[op1_reg].svec[i];
+        int32_t v2 = sim->temp_regs[op2_reg].svec[i];
+        sim->temp_regs[res_reg].uvec[i] = (v1 - (v2 * (int)(v1/v2)));
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpSMod)
+/* Signed modulo operation of Operand 1 modulo Operand 2. The sign of a non-0 result comes from Operand 2. */
+
+    assert(spirv_sim_type_is_integer(res_type->kind));
+    assert(res_type->is_signed);
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        int32_t v1 = sim->temp_regs[op1_reg].svec[i];
+        int32_t v2 = sim->temp_regs[op2_reg].svec[i];
+        sim->temp_regs[res_reg].uvec[i] = (v1 - (v2 * (int)floorf(v1/v2)));
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpFRem)
+/* Floating-point remainder operation of Operand 1 divided by Operand 2. The sign of a non-0 result comes from Operand 1. */
+
+    assert(spirv_sim_type_is_float(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        float v1 = sim->temp_regs[op1_reg].vec[i];
+        float v2 = sim->temp_regs[op2_reg].vec[i];
+        sim->temp_regs[res_reg].vec[i] = (v1 - (v2 * truncf(v1/v2)));
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpFMod)
+/* Floating-point modulo operation of Operand 1 divided by Operand 2. The sign of a non-0 result comes from Operand 2. */
+
+    assert(spirv_sim_type_is_float(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        float v1 = sim->temp_regs[op1_reg].vec[i];
+        float v2 = sim->temp_regs[op2_reg].vec[i];
+        sim->temp_regs[res_reg].vec[i] = (v1 - (v2 * floorf(v1/v2)));
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpVectorTimesScalar)
+/* Scale a floating-point vector. */
+
+    assert(spirv_sim_type_is_float(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].vec[i] = sim->temp_regs[op1_reg].vec[i] * sim->temp_regs[op2_reg].vec[0];
+    }
+
+OP_FUNC_END
+
+OP_FUNC_RES_2OP(SpvOpMatrixTimesScalar)
+/* Scale a floating-point matrix. */
+
+    assert(spirv_sim_type_is_float(res_type->kind));
+
+    for (int32_t i = 0; i < res_type->count; ++i) {
+        sim->temp_regs[res_reg].vec[i] = sim->temp_regs[op1_reg].vec[i] * sim->temp_regs[op2_reg].vec[0];
+    }
 
 OP_FUNC_END
 
@@ -179,6 +412,8 @@ OP_FUNC_BEGIN(SpvOpReturn)
 OP_FUNC_END
 
 #undef OP_FUNC_BEGIN
+#undef OP_FUNC_RES_1OP
+#undef OP_FUNC_RES_2OP
 #undef OP_FUNC_END
 
 void spirv_sim_step(SPIRV_simulator *sim) {
@@ -197,11 +432,58 @@ void spirv_sim_step(SPIRV_simulator *sim) {
 #define OP(kind)                        \
     case kind:                          \
         spirv_sim_op_##kind(sim, op);   \
-        break;      
+        break;
+#define OP_DEFAULT(kind)
 
     switch (op->op.kind) {
+        // miscellaneous instructions
+        OP_IGNORE(SpvOpNop)
+        OP_DEFAULT(SpvOpUndef)
+        OP_DEFAULT(SpvOpSizeOf)
+            
+        // memory instructions
+        OP_DEFAULT(SpvOpVariable)
+        OP_DEFAULT(SpvOpImageTexelPointer)
         OP(SpvOpLoad)
         OP(SpvOpStore)
+        OP_DEFAULT(SpvOpCopyMemory)
+        OP_DEFAULT(SpvOpCopyMemorySized)
+        OP_DEFAULT(SpvOpAccessChain)
+        OP_DEFAULT(SpvOpInBoundsAccessChain)
+        OP_DEFAULT(SpvOpPtrAccessChain)
+        OP_DEFAULT(SpvOpArrayLength)
+        OP_DEFAULT(SpvOpGenericPtrMemSemantics)
+        OP_DEFAULT(SpvOpInBoundsPtrAccessChain)
+            
+        // arithmetic instructions
+        OP(SpvOpSNegate)
+        OP(SpvOpFNegate)
+        OP(SpvOpIAdd)
+        OP(SpvOpFAdd)
+        OP(SpvOpISub)
+        OP(SpvOpFSub)
+        OP(SpvOpIMul)
+        OP(SpvOpFMul)
+        OP(SpvOpUDiv)
+        OP(SpvOpSDiv)
+        OP(SpvOpFDiv)
+        OP(SpvOpUMod)
+        OP(SpvOpSRem)
+        OP(SpvOpSMod)
+        OP(SpvOpFRem)
+        OP(SpvOpFMod)
+        OP(SpvOpVectorTimesScalar)
+        OP(SpvOpMatrixTimesScalar)
+        OP_DEFAULT(SpvOpVectorTimesMatrix)
+        OP_DEFAULT(SpvOpMatrixTimesVector)
+        OP_DEFAULT(SpvOpMatrixTimesMatrix)
+        OP_DEFAULT(SpvOpOuterProduct)
+        OP_DEFAULT(SpvOpDot)
+        OP_DEFAULT(SpvOpIAddCarry)
+        OP_DEFAULT(SpvOpISubBorrow)
+        OP_DEFAULT(SpvOpUMulExtended)
+        OP_DEFAULT(SpvOpSMulExtended)
+
         OP(SpvOpReturn)
 
         default:
@@ -209,6 +491,8 @@ void spirv_sim_step(SPIRV_simulator *sim) {
     }
 
 #undef OP_IGNORE
+#undef OP
+#undef OP_DEFAULT
 
     spirv_bin_opcode_next(sim->module->spirv_bin);
 }
