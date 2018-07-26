36d01ac96349ea17987b5ed31c703e1a9244b152
Johan Smet <johan.smet@justcode.be>
2018-07-26 21:38:02 +0200

    Add unit tests for integer arithmetic instructions

--- tests/test_spirv_sim.c
+++ tests/test_spirv_sim.c
@@ -27,6 +27,18 @@ static inline float fremf(float v1, float v2) {
     return (v1 - (v2 * floorf(v1/v2)));
 }
 
+static inline int32_t smod(int32_t v1, int32_t v2) {
+    return (v1 - (v2 * truncf((float) v1 / v2)));
+}
+
+static inline int32_t srem(int32_t v1, int32_t v2) {
+    return (v1 - (v2 * floorf((float) v1 / v2)));
+}
+
+static inline uint32_t urem(uint32_t v1, uint32_t v2) {
+    return (v1 - (v2 * floorf((float) v1 / v2)));
+}
+
 static inline void spirv_common_header(SPIRV_binary *bin) {
     SPIRV_OP(bin, SpvOpCapability,  SpvCapabilityShader);
     SPIRV_OP(bin, SpvOpExtInstImport, ID(1), S('G','L','S','L'), S('.','s','t','d'), S('.','4','5','0'), S(0,0,0,0));
@@ -52,7 +64,7 @@ static inline void spirv_common_types(SPIRV_binary *bin, uint32_t type_flags) {
         SPIRV_OP(bin, SpvOpTypePointer, ID(23), SpvStorageClassInput, ID(21));
     }
     
-    if (type_flags & TEST_TYPE_INT32) {
+    if (type_flags & TEST_TYPE_UINT32) {
         SPIRV_OP(bin, SpvOpTypeInt, ID(30), 32, 0);
         SPIRV_OP(bin, SpvOpTypeVector, ID(31), ID(30), 4);
         SPIRV_OP(bin, SpvOpTypeMatrix, ID(32), ID(31), 4);
@@ -73,11 +85,29 @@ static inline void spirv_common_function_footer(SPIRV_binary *bin) {
 #define ASSERT_REGISTER_VEC4(sim, id, op, e0, e1, e2, e3) { \
     SimRegister *reg = spirv_sim_register_by_id(sim, id);   \
     munit_assert_not_null(reg);                             \
-    munit_assert_float(reg->vec[0], op, e0);       \
-    munit_assert_float(reg->vec[1], op, e1);       \
-    munit_assert_float(reg->vec[2], op, e2);       \
-    munit_assert_float(reg->vec[3], op, e3);       \
+    munit_assert_float(reg->vec[0], op, (e0));       \
+    munit_assert_float(reg->vec[1], op, (e1));       \
+    munit_assert_float(reg->vec[2], op, (e2));       \
+    munit_assert_float(reg->vec[3], op, (e3));       \
 }
+
+#define ASSERT_REGISTER_SVEC4(sim, id, op, e0, e1, e2, e3) { \
+    SimRegister *reg = spirv_sim_register_by_id(sim, id);   \
+    munit_assert_not_null(reg);                             \
+    munit_assert_int32(reg->svec[0], op, (e0));       \
+    munit_assert_int32(reg->svec[1], op, (e1));       \
+    munit_assert_int32(reg->svec[2], op, (e2));       \
+    munit_assert_int32(reg->svec[3], op, (e3));       \
+}
+
+#define ASSERT_REGISTER_UVEC4(sim, id, op, e0, e1, e2, e3) { \
+    SimRegister *reg = spirv_sim_register_by_id(sim, id);   \
+    munit_assert_not_null(reg);                             \
+    munit_assert_uint32(reg->svec[0], op, (e0));       \
+    munit_assert_uint32(reg->svec[1], op, (e1));       \
+    munit_assert_uint32(reg->svec[2], op, (e2));       \
+    munit_assert_uint32(reg->svec[3], op, (e3));       \
+    }
 
 MunitResult test_arithmetic_float32(const MunitParameter params[], void* user_data_or_fixture) {
     
@@ -150,7 +180,137 @@ MunitResult test_arithmetic_float32(const MunitParameter params[], void* user_da
     return MUNIT_OK;
 }
 
+MunitResult test_arithmetic_int32(const MunitParameter params[], void* user_data_or_fixture) {
+    
+    /* prepare binary */
+    SPIRV_binary spirv_bin;
+    spirv_bin_init(&spirv_bin, 1, 0);
+    
+    spirv_common_header(&spirv_bin);
+    SPIRV_OP(&spirv_bin, SpvOpDecorate, ID(40), SpvDecorationLocation, 0);
+    SPIRV_OP(&spirv_bin, SpvOpDecorate, ID(41), SpvDecorationLocation, 1);
+    spirv_common_types(&spirv_bin, TEST_TYPE_INT32);
+    SPIRV_OP(&spirv_bin, SpvOpVariable, ID(23), ID(40), SpvStorageClassInput);
+    SPIRV_OP(&spirv_bin, SpvOpVariable, ID(23), ID(41), SpvStorageClassInput);
+    spirv_common_function_header(&spirv_bin);
+    SPIRV_OP(&spirv_bin, SpvOpLoad, ID(21), ID(60), ID(40));
+    SPIRV_OP(&spirv_bin, SpvOpLoad, ID(21), ID(61), ID(41));
+    SPIRV_OP(&spirv_bin, SpvOpSNegate, ID(21), ID(62), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpIAdd, ID(21), ID(63), ID(61), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpISub, ID(21), ID(64), ID(61), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpIMul, ID(21), ID(65), ID(61), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpSDiv, ID(21), ID(66), ID(61), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpSRem, ID(21), ID(67), ID(61), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpSRem, ID(21), ID(68), ID(61), ID(62));
+    SPIRV_OP(&spirv_bin, SpvOpSMod, ID(21), ID(69), ID(61), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpSMod, ID(21), ID(70), ID(61), ID(62));
+    spirv_common_function_footer(&spirv_bin);
+    spirv_bin.header.bound_ids = 71;
+    spirv_bin_finalize(&spirv_bin);
+    
+    /* prepare simulator */
+    SPIRV_module spirv_module;
+    spirv_module_load(&spirv_module, &spirv_bin);
+    
+    SPIRV_simulator spirv_sim;
+    spirv_sim_init(&spirv_sim, &spirv_module);
+    int32_t data1[4] = {1, 2, 3, 4};
+    int32_t data2[4] = {3, 6, 8, 11};
+    spirv_sim_variable_associate_data(&spirv_sim, VarKindInput, VarInterfaceLocation, 0, (uint8_t *) data1, sizeof(data1));
+    spirv_sim_variable_associate_data(&spirv_sim, VarKindInput, VarInterfaceLocation, 1, (uint8_t *) data2, sizeof(data2));
+    
+    /* run simulator */
+    while (!spirv_sim.finished && !spirv_sim.error_msg) {
+        spirv_sim_step(&spirv_sim);
+    }
+    
+    /* check registers */
+    ASSERT_REGISTER_SVEC4(&spirv_sim, ID(62), ==,                        /* OpSNegate */
+                         -data1[0], -data1[1], -data1[2], -data1[3]);
+    ASSERT_REGISTER_SVEC4(&spirv_sim, ID(63), ==,                        /* OpIAdd */
+                         data2[0] + data1[0], data2[1] + data1[1], data2[2] + data1[2], data2[3] + data1[3]);
+    ASSERT_REGISTER_SVEC4(&spirv_sim, ID(64), ==,                        /* OpISub */
+                         data2[0] - data1[0], data2[1] - data1[1], data2[2] - data1[2], data2[3] - data1[3]);
+    ASSERT_REGISTER_SVEC4(&spirv_sim, ID(65), ==,                        /* OpIMul */
+                         data2[0] * data1[0], data2[1] * data1[1], data2[2] * data1[2], data2[3] * data1[3]);
+    ASSERT_REGISTER_SVEC4(&spirv_sim, ID(66), ==,                        /* OpSDiv */
+                         data2[0] / data1[0], data2[1] / data1[1], data2[2] / data1[2], data2[3] / data1[3]);
+    ASSERT_REGISTER_SVEC4(&spirv_sim, ID(67), ==,                        /* OpSRem */
+                         smod(data2[0], data1[0]), smod(data2[1], data1[1]),
+                         smod(data2[2], data1[2]), smod(data2[3], data1[3]));
+    ASSERT_REGISTER_SVEC4(&spirv_sim, ID(68), ==,                        /* OpSRem */
+                         smod(data2[0], -data1[0]), smod(data2[1], -data1[1]),
+                         smod(data2[2], -data1[2]), smod(data2[3], -data1[3]));
+    ASSERT_REGISTER_SVEC4(&spirv_sim, ID(69), ==,                        /* OpSMod */
+                         srem(data2[0], data1[0]), srem(data2[1], data1[1]),
+                         srem(data2[2], data1[2]), srem(data2[3], data1[3]));
+    ASSERT_REGISTER_SVEC4(&spirv_sim, ID(70), ==,                        /* OpSMod */
+                         srem(data2[0], -data1[0]), srem(data2[1], -data1[1]),
+                         srem(data2[2], -data1[2]), srem(data2[3], -data1[3]));
+
+    return MUNIT_OK;
+}
+
+MunitResult test_arithmetic_uint32(const MunitParameter params[], void* user_data_or_fixture) {
+    
+    /* prepare binary */
+    SPIRV_binary spirv_bin;
+    spirv_bin_init(&spirv_bin, 1, 0);
+    
+    spirv_common_header(&spirv_bin);
+    SPIRV_OP(&spirv_bin, SpvOpDecorate, ID(40), SpvDecorationLocation, 0);
+    SPIRV_OP(&spirv_bin, SpvOpDecorate, ID(41), SpvDecorationLocation, 1);
+    spirv_common_types(&spirv_bin, TEST_TYPE_UINT32);
+    SPIRV_OP(&spirv_bin, SpvOpVariable, ID(33), ID(40), SpvStorageClassInput);
+    SPIRV_OP(&spirv_bin, SpvOpVariable, ID(33), ID(41), SpvStorageClassInput);
+    spirv_common_function_header(&spirv_bin);
+    SPIRV_OP(&spirv_bin, SpvOpLoad, ID(31), ID(60), ID(40));
+    SPIRV_OP(&spirv_bin, SpvOpLoad, ID(31), ID(61), ID(41));
+    SPIRV_OP(&spirv_bin, SpvOpIAdd, ID(31), ID(62), ID(61), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpISub, ID(31), ID(63), ID(61), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpIMul, ID(31), ID(64), ID(61), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpUDiv, ID(31), ID(65), ID(61), ID(60));
+    SPIRV_OP(&spirv_bin, SpvOpUMod, ID(31), ID(66), ID(61), ID(60));
+    spirv_common_function_footer(&spirv_bin);
+    spirv_bin.header.bound_ids = 67;
+    spirv_bin_finalize(&spirv_bin);
+    
+    /* prepare simulator */
+    SPIRV_module spirv_module;
+    spirv_module_load(&spirv_module, &spirv_bin);
+    
+    SPIRV_simulator spirv_sim;
+    spirv_sim_init(&spirv_sim, &spirv_module);
+    uint32_t data1[4] = {1, 2, 3, 4};
+    uint32_t data2[4] = {3, 6, 8, 11};
+    spirv_sim_variable_associate_data(&spirv_sim, VarKindInput, VarInterfaceLocation, 0, (uint8_t *) data1, sizeof(data1));
+    spirv_sim_variable_associate_data(&spirv_sim, VarKindInput, VarInterfaceLocation, 1, (uint8_t *) data2, sizeof(data2));
+    
+    /* run simulator */
+    while (!spirv_sim.finished && !spirv_sim.error_msg) {
+        spirv_sim_step(&spirv_sim);
+    }
+    
+    /* check registers */
+    ASSERT_REGISTER_UVEC4(&spirv_sim, ID(62), ==,                        /* OpIAdd */
+                          data2[0] + data1[0], data2[1] + data1[1], data2[2] + data1[2], data2[3] + data1[3]);
+    ASSERT_REGISTER_UVEC4(&spirv_sim, ID(63), ==,                        /* OpISub */
+                          data2[0] - data1[0], data2[1] - data1[1], data2[2] - data1[2], data2[3] - data1[3]);
+    ASSERT_REGISTER_UVEC4(&spirv_sim, ID(64), ==,                        /* OpIMul */
+                          data2[0] * data1[0], data2[1] * data1[1], data2[2] * data1[2], data2[3] * data1[3]);
+    ASSERT_REGISTER_UVEC4(&spirv_sim, ID(65), ==,                        /* OpUDiv */
+                          data2[0] / data1[0], data2[1] / data1[1], data2[2] / data1[2], data2[3] / data1[3]);
+    ASSERT_REGISTER_UVEC4(&spirv_sim, ID(66), ==,                        /* OpUMod */
+                          urem(data2[0], data1[0]), urem(data2[1], data1[1]),
+                          urem(data2[2], data1[2]), urem(data2[3], data1[3]));
+
+    return MUNIT_OK;
+}
+
+
 MunitTest spirv_sim_tests[] = {
     { "/arithmetic_float32", test_arithmetic_float32, NULL, NULL,  MUNIT_TEST_OPTION_NONE, NULL },
+    { "/arithmetic_int32", test_arithmetic_int32, NULL, NULL,  MUNIT_TEST_OPTION_NONE, NULL },
+    { "/arithmetic_uint32", test_arithmetic_uint32, NULL, NULL,  MUNIT_TEST_OPTION_NONE, NULL },
     { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
